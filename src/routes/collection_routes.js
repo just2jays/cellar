57dfc11ab1f56a999e6402ad7516d28ac67f1067
JJ Guaragno <just2jays@gmail.com>
2017-06-27 13:29:07 -0400

    asdf

--- src/routes/collection_routes.js
+++ src/routes/collection_routes.js
@@ -2,14 +2,10 @@ var request = require('request');
 
 module.exports = function(app, db) {
     app.post('/collection', (req, res) => {
-        // You'll create your note here.
-        console.log(req.body)
-        res.send('Hello')
+
     });
 
     app.get('/collection/:name', (req, res) => {
-        //console.log(req);
-        // https://archive.org/advancedsearch.php?q=collection%3A%28DeadAndCompany%29&fl%5B%5D=avg_rating&fl%5B%5D=collection&fl%5B%5D=date&fl%5B%5D=description&fl%5B%5D=downloads&fl%5B%5D=identifier&fl%5B%5D=title&sort%5B%5D=addeddate+desc&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json
         request({
             url: 'https://archive.org/advancedsearch.php?q=collection%3A%28'+req.params.name+'%29&fl%5B%5D=avg_rating&fl%5B%5D=collection&fl%5B%5D=date&fl%5B%5D=description&fl%5B%5D=downloads&fl%5B%5D=identifier&fl%5B%5D=title&sort%5B%5D=addeddate+desc&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json',
             headers: {
