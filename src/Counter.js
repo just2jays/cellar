0c37274dc5597de54c55bb5ad66b60aa912d7614
JJ Guaragno <just2jays@gmail.com>
2017-06-27 18:30:28 -0400

    Actual decent progress

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react'
 
 /**
  * A counter button: tap the button to increase the count.
@@ -11,26 +12,9 @@ class Counter extends React.Component {
     };
   }
  
-componentDidMount() {
-  fetch('/github')
-  .then(response => response.json())
-  .then((data) => {
-    const repos = data.map((repo) =>
-      <p key={repo.id}>{repo.name}</p>
-    );
-    this.setState({ repos })
-  });
-}
-
   render() {
     return (
-      <button
-        onClick={() => {
-          this.setState({ count: this.state.count + 1 });
-        }}
-      >
-        Count: {this.state.count}
-      </button>
+      <Button primary>Primary</Button>
     );
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
+import SongList from './SongList';
  
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    React.createElement(Counter),
+    React.createElement(SongList),
     document.getElementById('mount')
   );
 });
