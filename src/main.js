c7d051a13ed36311749b5a231a8f57d320abe03e
JJ Guaragno <just2jays@gmail.com>
2017-06-28 17:54:09 -0400

    from mount to app

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
 import SongList from './SongList';
  
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
     React.createElement(SongList),
-    document.getElementById('mount')
+    document.getElementById('app')
   );
 });
