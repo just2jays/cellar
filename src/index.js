2d924395fdd6a6dc2ffdd58aa9083b0c38196ccd
JJ Guaragno <just2jays@gmail.com>
2017-06-29 16:00:50 -0400

    add global app browserrouter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
     document.getElementById('app')
   );
 });
