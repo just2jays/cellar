eb40ed6afe22be57a9ade26c7ee61c9277297e2e
JJ Guaragno <just2jays@gmail.com>
2017-07-06 15:47:19 -0400

    Use React Howler Component
    Switched from HTML5 <Audio> DOM element to the `react-howler` component
    
    https://github.com/thangngoc89/react-howler

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -1,24 +1,45 @@
 import React from 'react';
+import ReactHowler from 'react-howler';
+import { Button } from 'semantic-ui-react';
 import './main.scss';
 
 class AudioPlayer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        playingTrack: this.props.track
+        playingTrack: this.props.track,
+        playing: true,
+        mute: false,
+        volume: 1.0
     };
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handlePause = this.handlePause.bind(this);
+  }
+
+  handlePlay() {
+    this.setState({
+      playing: true
+    });
+  }
+
+  handlePause() {
+    this.setState({
+      playing: false
+    });
   }
 
   render() {
     return (
         <div className="player-container">
-        <audio
-            src={this.props.track}
-            autoPlay
-            controls
-        >
-        Your browser does not support the <code>audio</code> element.
-        </audio>
+          <div className="player-container-inner">
+            <ReactHowler
+              html5={true}
+              src={[this.props.track]}
+              playing={this.state.playing}
+            />
+            <Button onClick={this.handlePause} content='Pause' icon='pause' labelPosition='left' />
+            <Button onClick={this.handlePlay} content='Play' icon='right arrow' labelPosition='right' />
+          </div>
         </div>
     );
   }
