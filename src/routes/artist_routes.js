fbc07d1c92f5b4463b4de77774ba952e63db83c0
JJ Guaragno <just2jays@gmail.com>
2017-07-18 11:44:34 -0400

    hi pineapple

--- src/routes/artist_routes.js
+++ src/routes/artist_routes.js
@@ -1,4 +1,4 @@
-var lastfmConfig = require('../../config/lastfm.js');
+var config = require('../../config/config.js');
 var request = require('request');
 
 module.exports = function(app, db) {
@@ -8,7 +8,7 @@ module.exports = function(app, db) {
 
     app.get('/artist/:name', (req, res) => {
         request({
-            url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist='+encodeURIComponent(req.params.name)+'&api_key='+lastfmConfig.API_KEY+'&format=json',
+            url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist='+encodeURIComponent(req.params.name)+'&api_key='+config.lastfm.API_KEY+'&format=json',
             headers: {
                 'user-agent': 'node.js',
             },
