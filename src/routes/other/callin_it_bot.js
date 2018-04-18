180f7ac8ce7049dc75748506e44f0f89da0f07b5
JJ <just2jays@gmail.com>
2018-04-17 22:10:51 -0400

    ok go

--- src/routes/other/callin_it_bot.js
+++ src/routes/other/callin_it_bot.js
@@ -32,6 +32,6 @@ module.exports = function(app, db) {
             }
           });
           
-          client.login(config.discordConfig.TOKEN);
+          client.login(config.discord.TOKEN);
     });
 };
