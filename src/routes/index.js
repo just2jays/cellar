144de7049c68ed7525537a418fba7f04ef64bd32
JJ <just2jays@gmail.com>
2018-04-17 21:42:24 -0400

    try again

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const collectionRoutes = require('./collection_routes');
 const showRoutes = require('./show_routes');
 const artistRoutes = require('./artist_routes');
+const callin_it_bot = require('./other/callin_it_bot');
 
 module.exports = function(app, db) {
   collectionRoutes(app, db);
   showRoutes(app, db);
   artistRoutes(app, db);
+  callin_it_bot(app, db);
 };
