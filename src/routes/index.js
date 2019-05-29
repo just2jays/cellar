c8819875a99f2f59eb29988b68e24adaa329e94c
JJ G <jguaragno@mvpub.com>
2019-05-29 16:53:03 -0400

    trying rando

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const collectionRoutes = require('./collection_routes');
 const showRoutes = require('./show_routes');
 const artistRoutes = require('./artist_routes');
 const callin_it_bot = require('./other/callin_it_bot');
+const score_bot = require('./other/score');
 
 module.exports = function(app, db) {
   collectionRoutes(app, db);
   showRoutes(app, db);
   artistRoutes(app, db);
+  score_bot(app, db);
 };
