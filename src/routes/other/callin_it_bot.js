b20e2cef2cf3f62c6e4f021c28a0d3b1046c791a
JJ <jguaragno@mansueto.com>
2021-05-29 13:09:27 -0400

    asdf

--- src/routes/other/callin_it_bot.js
+++ src/routes/other/callin_it_bot.js
@@ -54,12 +54,21 @@ module.exports = function(app, db) {
         //     // });
         // });
 
-        console.log('🔶 fetching for user: ', '\n', user);
+        // console.log('🔶 fetching for user: ', '\n', user);
+
 
         userStatsRef.child(user).once('value').then( function(snap) {
             // const user = snap.val()
             // const userKey = snap.key
-            console.log('✅ snap val', '\n', snap.val());
+            // console.log('✅ snap val', '\n', snap.val());
+            let snapArray = [...snap.val()];
+            console.log('✅ snapArray', '\n', snapArray);
+            snapshot.forEach(snap => {
+                // var k = userSnapshot.key;
+                // var id = userSnapshot.val().AssignedID;
+                // var name = userSnapshot.val().Name;
+                console.log('🚨 snap', '\n', snap.val());
+            })
         })
         // userStatsRef.child(user).get().then((snapshot) => {
         //     if (snapshot.exists()) {
